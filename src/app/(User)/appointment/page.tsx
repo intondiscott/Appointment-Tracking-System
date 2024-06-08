19198e5f6ff5afab889c617598c8c16bf393cc29
Scotty <intondiscott@gmail.com>
2024-06-08 09:41:33 -0400

    Updated appointment system

--- src/app/(User)/appointment/page.tsx
+++ src/app/(User)/appointment/page.tsx
@@ -1,18 +1,28 @@
 export default async function AppointmentPage() {
   const getData = async () => {
-    const res = await fetch(
-      "http://localhost:3000/api/accounts/1/appointments",
-      { next: { revalidate: 10 } }
-    );
+    const res = await fetch("http://localhost:3000/api/accounts/", {
+      next: { revalidate: 10 },
+    });
     return res.json();
   };
   const data = await getData();
   return (
     <div>
-      {data?.appointments === "yes" ? (
-        <div className='rounded-full bg-red-700 w-[40px] h-[40px]'></div>
-      ) : (
-        ""
+      Appointments:
+      {data.map((res: any, idx: number) =>
+        res.accounts.map(
+          (items: any) =>
+            items.appointment.length > 0 && (
+              <div key={idx} className='bg-gray-300 m-10 rounded'>
+                <p>
+                  <strong>Name:</strong> {items.name}
+                </p>
+                <p>
+                  <strong>Appointment Description:</strong> {items.appointment}
+                </p>
+              </div>
+            )
+        )
       )}
     </div>
   );
