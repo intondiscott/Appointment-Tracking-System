70f76608ed42eda7328714de4c8feffcfecd7e8d
Scotty <intondiscott@gmail.com>
2024-07-02 15:16:18 -0400

    updated project

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,22 +1,32 @@
-import { FC } from 'react';
-import Link from 'next/link';
-
-interface HeaderProps {
-  onLoginClick: () => void;
-}
-
-const Header: FC<HeaderProps> = ({ onLoginClick }) => {
+import { FC } from "react";
+import Link from "next/link";
+const isLoggedIn: boolean = false;
+const Header = () => {
   return (
-    <header className="bg-gray-800 text-white sticky top-0 flex items-center justify-between px-4 py-5">
-      <nav className="flex justify-between w-full">
-        <ul className="flex space-x-8">
-          <li><Link href="/">Home</Link></li>
-          <li><Link href="/about">About</Link></li>
-          <li><Link href="/solution">Solution</Link></li>
+    <header className='bg-gray-800 text-white sticky top-0 flex items-center justify-between px-4 py-5'>
+      <nav className='flex justify-between w-full'>
+        <ul className='flex space-x-8'>
+          <li>
+            <Link href='/'>Home</Link>
+          </li>
+          <li>
+            <Link href='/about'>About</Link>
+          </li>
+          <li>
+            <Link href='/solution'>Solution</Link>
+          </li>
         </ul>
-        <ul className="flex space-x-8">
-          <li><button onClick={onLoginClick} className="hover:underline">Login</button></li>
-          <li><Link href="/request-demo" className="hover:underline">Request for Demo</Link></li>
+        <ul className='flex space-x-8'>
+          <li>
+            <Link href='/Login' className='hover:underline'>
+              Login
+            </Link>
+          </li>
+          <li>
+            <Link href='/request-demo' className='hover:underline'>
+              Request for Demo
+            </Link>
+          </li>
         </ul>
       </nav>
     </header>
