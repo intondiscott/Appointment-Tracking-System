4a50d709d2cac19c2e67df0d659cf29ae1ed15f0
Intondiscott <intondiscott@gmail.com>
2024-10-27 11:26:51 -0400

    env variables updated

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,11 @@
 // This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-if (!process.env.MONGODB_URI) {
+if (!process.env.MONGO_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
 }
 
-const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGO_URI;
 const options = {
   serverApi: {
     version: ServerApiVersion.v1,
